3de6b0cdfbf4b6cece709e6f20e3ab6470ec07a9
TC Yang <yangtongchee@hotmail.com>
2021-08-19 08:05:44 -0400

    console

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -1,6 +1,9 @@
 //this creates a function named "fight"
 function fight(){
 var playerName = window.prompt("What is your robot's name?");
+
+window.alert(playerName);
+
 //console log
 console.log(playerName);
 
@@ -9,6 +12,15 @@ console.log("This logs a string, good for leaving yourself a message");
 console.log(10 + 10);
 // what is this?
 console.log("Our robot's name is " + playerName);
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+var name="TC";
+console.log(name);
 }
 
 /*fight is the function*/
